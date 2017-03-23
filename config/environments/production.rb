0d4a5d3649b542064a9141ac62899471cced9112
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-03-23 21:57:09 +1100

    Changes to environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,14 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.serve_static_files = true # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-
+  config.assets.compile = true
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
