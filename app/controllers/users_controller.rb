26d35921b4e2f5be504ecbc0296e1354edddfd32
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-03-24 12:32:39 +1100

    Add auto-log in after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(clean_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
